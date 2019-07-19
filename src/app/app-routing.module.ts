f129cb2475431f57eb2f83af80d777d7833b14b5
nader.hatami <nader.hatami@slalom.com>
2019-07-19 19:31:40 -0400

    some updates here and there

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { PlanetComponent } from './planet/planet.component';
 import { FilmComponent } from 'src/app/film/film.component';
 import { StarshipComponent } from 'src/app/starship/starship.component';
 import { VehicleComponent } from 'src/app/vehicle/vehicle.component';
+import { CharacterListComponent } from 'src/app/character-list/character-list.component';
 
 const routes = [
   {
@@ -23,13 +24,14 @@ const routes = [
     loadChildren:
       'src/app/create-character/create-character.module#CreateCharacterModule'
   },
+  { path: 'character-list', component: CharacterListComponent },
   { path: 'character', component: CharacterComponent },
   { path: 'character/:url', component: CharacterComponent },
   { path: 'starship/:url', component: StarshipComponent },
   { path: 'vehicle/:url', component: VehicleComponent },
   { path: 'planet/:url', component: PlanetComponent },
   { path: 'film/:url', component: FilmComponent },
-  { path: '**', redirectTo: '/characters' }
+  { path: '**', redirectTo: '/character-list' }
 ];
 
 @NgModule({
